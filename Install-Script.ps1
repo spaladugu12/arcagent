de483a4eebbb28d99e35e038c205ec718a708ea4
spaladugu12 <99915055+spaladugu12@users.noreply.github.com>
2026-02-01 14:42:01 -0600

    Refactor Install-Script to improve logon script creation
    Updated the Install-Script.ps1 to create a logon script with actual values substituted and added a sleep command before running the connect command.

--- Install-Script.ps1
+++ Install-Script.ps1
@@ -8,18 +8,10 @@ param (
     [string]$adminUsername
 )
 
-[System.Environment]::SetEnvironmentVariable('subscriptionId', $subscriptionId,[System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable('appId', $appId,[System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable('password', $password,[System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable('tenantId', $tenantId,[System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable('resourceGroup', $resourceGroup,[System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable('location', $location,[System.EnvironmentVariableTarget]::Machine)
-[System.Environment]::SetEnvironmentVariable('adminUsername', $adminUsername,[System.EnvironmentVariableTarget]::Machine)
-
 New-Item -Path "C:\" -Name "tmp" -ItemType "directory" -Force
 
-# Creating PowerShell Logon Script
-$LogonScript = @'
+# Creating PowerShell Logon Script with actual values substituted
+$LogonScript = @"
 Start-Transcript -Path C:\tmp\LogonScript.log
 
 ## Configure the OS to allow Azure Arc Agent to be deploy on an Azure VM
@@ -33,26 +25,28 @@ New-NetFirewallRule -Name BlockAzureIMDS -DisplayName "Block access to Azure IMD
 
 Write-Host "Onboarding to Azure Arc"
 # Download the package
-function download() {$ProgressPreference="SilentlyContinue"; Invoke-WebRequest -Uri https://aka.ms/AzureConnectedMachineAgent -OutFile AzureConnectedMachineAgent.msi}
+function download() {`$ProgressPreference="SilentlyContinue"; Invoke-WebRequest -Uri https://aka.ms/AzureConnectedMachineAgent -OutFile AzureConnectedMachineAgent.msi}
 download
 
 # Install the package
 msiexec /i AzureConnectedMachineAgent.msi /l*v installationlog.txt /qn | Out-String
+Start-Sleep -Seconds 30
 
 # Run connect command
- & "$env:ProgramFiles\AzureConnectedMachineAgent\azcmagent.exe" connect `
- --service-principal-id $env:appId `
- --service-principal-secret $env:password `
- --resource-group $env:resourceGroup `
- --tenant-id $env:tenantId `
- --location $env:location `
- --subscription-id $env:subscriptionId `
- --tags "Project=jumpstart_azure_arc_servers" `
+`$env:MSFT_ARC_TEST = "true"
+& "`$env:ProgramFiles\AzureConnectedMachineAgent\azcmagent.exe" connect ``
+ --service-principal-id '$appId' ``
+ --service-principal-secret '$password' ``
+ --resource-group '$resourceGroup' ``
+ --tenant-id '$tenantId' ``
+ --location '$location' ``
+ --subscription-id '$subscriptionId' ``
+ --tags "Project=jumpstart_azure_arc_servers" ``
  --correlation-id "d009f5dd-dba8-4ac7-bac9-b54ef3a6671a"
 
-Unregister-ScheduledTask -TaskName "LogonScript" -Confirm:$False
+Unregister-ScheduledTask -TaskName "LogonScript" -Confirm:`$False
 Stop-Process -Name powershell -Force
-'@ > C:\tmp\LogonScript.ps1
+"@ | Out-File -FilePath C:\tmp\LogonScript.ps1 -Force
 
 # Creating LogonScript Windows Scheduled Task
 $Trigger = New-ScheduledTaskTrigger -AtStartup
